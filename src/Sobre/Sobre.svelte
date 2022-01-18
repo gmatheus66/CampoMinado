5f7bb74315e5291a0ad8a3df8644b302ef40aff6
1thomas-santos <83986386+1thomas-santos@users.noreply.github.com>
2022-01-18 20:43:01 -0300

    Update Sobre.svelte

--- src/Sobre/Sobre.svelte
+++ src/Sobre/Sobre.svelte
@@ -2,4 +2,28 @@
 
 </script>
 
-<h2>Teste</h2>
+<h2>Este jogo foi criado com o intuito de expor os conhecimentos adquiridos sobre Lógica de programação imperativa com o uso do Node.js </h2>
+<p>
+  Seu objetivo consiste em revelar as casas do tabuleiro sem as bombas.
+</p>
+
+<p>
+  Seu desenvolvimento foi liderado por Matheus Gonçalves, auxiliado por por Thomas Santos e Lázaro Henrique
+</p>
+
+<p> 
+  Matheus Gonçalves - <a href="https://github.com/gmatheus66">https://github.com/gmatheus66</a>
+</p>
+
+<p> 
+  Thomas Santos - <a href="https://github.com/1thomas-santos">https://github.com/1thomas-santos</a>
+</p>
+
+<p> 
+  Lazaro Henrique - <a href="https://github.com/lazhenri">https://github.com/lazhenri</a>
+</p> 
+
+
+
+  
+  
